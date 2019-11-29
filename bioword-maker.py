3bd88414d49355faf40d4c2a29a64f2940428460
Tobias O'Leary <tobiasoleary@mac.com>
2019-11-29 14:41:15 -0500

    Adds way of generating encodings for medical terminology

---
+++ bioword-maker.py
@@ -0,0 +1,79 @@
+#bioword-finder.py
+
+# This file produces a text file of all biowords in the data for vectorization.
+
+import sys
+import os
+
+
+#Updates Classpath to fix dependency.
+new_classpath = os.path.join(os.path.dirname(__file__), "lib/JAVA_CLASSPATH")
+if "CLASSPATH" in os.environ:
+	new_classpath = os.environ['CLASSPATH'] + ":" + new_classpath
+
+os.environ["CLASSPATH"] = new_classpath
+
+import pandas as pd
+import math
+
+from pandas import ExcelFile
+from nltk.tokenize.stanford import StanfordTokenizer 
+import Levenshtein as leven
+from collections import Counter
+ 
+INPUT_FILE = f"{os.path.dirname(os.path.abspath(__file__))}/txpl_project.xlsx"
+OUTPUT_FILENAME = "biowords_" + os.path.splitext(os.path.basename(INPUT_FILE))[0] + ".txt"
+OUTPUT_FILE = f"{os.path.dirname(os.path.abspath(__file__))}/{OUTPUT_FILENAME}"
+print(f"Reading {INPUT_FILE}")
+df = pd.read_excel(INPUT_FILE)
+
+COLUMNS_TO_PROCESS = ['SPECOTH', 'CHD_OTHSP', 'SURGERY_HISTORY', 'LSNYHA_T', 'LSRHFCL_T', 'SLINODS']
+# COLUMNS_TO_PROCESS = ['SPECOTH']
+
+vocab = Counter()
+stk = StanfordTokenizer() #Dependencies in the lib folder, ADD IT TO YOUR `CLASSPATH` env variable
+for col_name in COLUMNS_TO_PROCESS:
+  print(f"Processing {col_name}...")
+  for index, item in enumerate(df[col_name]):
+    if type(item) is str:
+      vocab.update(stk.tokenize(item))
+    if (index % 500) == 0:
+      print(f"  at index {index}")
+
+
+print(f"Vocab Size: {len(vocab)}")
+
+print("\n".join(vocab.keys()))
+
+with open(OUTPUT_FILE, 'w') as f:
+	f.write("\n".join(vocab.keys()))
+
+
+# print(f"Specoth Count: {len(specoth)}")
+#
+# for keyword in keywords:
+#   for index, item in enumerate(specoth):
+#     tokens = stk.tokenize(item)
+#     for t in tokens:
+#       editdist = leven.distance(t.lower(), 'heterotaxy'.lower())
+#       if editdist < 4:
+#         print(f"{index} - {item} - {t}")
+#         print(f"EDIT DISTANCE: {editdist}")
+#
+# print("\n-----------\n")
+# print("CHD_OTHSP")
+# chd_othsp = [item for item in df["CHD_OTHSP"].to_list() if type(item) is str]
+# print(f"Chd_Othsp Count: {len(chd_othsp)}")
+# # print("\n".join(chd_othsp))
+#
+# for index, item in enumerate(chd_othsp):
+#   tokens = stk.tokenize(item)
+#   for t in tokens:
+#     editdist = leven.distance(t.lower(), 'heterotaxy'.lower())
+#     if editdist < 4:
+#       print(f"{index} - {item} - {t}")
+#       print(f"EDIT DISTANCE: {editdist}")
+
+
+
+
---
+++ biowords-to-vects.py
@@ -0,0 +1,40 @@
+import math
+import os
+import time
+
+import fasttext
+import psutil
+import numpy as np
+import json
+
+def get_memusage_gb():
+    as_bytes = psutil.Process(os.getpid()).memory_info().rss
+    return as_bytes / (1024 ** 3)
+
+
+# Load the word vec model.
+started = time.time()
+MODEL_PATH = f"{os.environ['USER_SCRATCH']}/BioWordVec_PubMed_MIMICIII_d200.bin"
+print(f"Starting to Load Model")
+model = fasttext.load_model(MODEL_PATH)
+print(model)
+print('loaded in %ds' % (time.time() - started))
+print(f"GB MEM Useage: {get_memusage_gb()}")
+
+
+# Load input File one word per line
+with open('biowords_txpl_project.txt') as f:
+	read_data = f.read()
+
+words = read_data.splitlines()
+
+word_to_vect = {}
+for word in words:
+   vect = model.get_word_vector(word)
+   word_to_vect[word] = vect
+   
+with open('biowords-to-vects.json', 'w') as f:
+  json.dump(word_to_vect, f)
+  
+
+
