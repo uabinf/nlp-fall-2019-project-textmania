7f1aa098aca6397222414c6dbd479eb949fec11a
Tobias O'Leary <tobiasoleary@mac.com>
2019-11-29 20:31:40 -0500

    Updates setup.bash.sh with BioWordVec download ability

--- setup.bash.sh
+++ setup.bash.sh
@@ -4,9 +4,11 @@
 if [ ${#SSH_CONNECTION} -gt 0 ]; then
   module load Python/3.6.3-intel-2017a
 fi
+
 # Install virtualenv
 pip install virtualenv 
 
+# Setup virtualenv
 if [ ! -d ./venv ]; then
   virtualenv venv
 fi
@@ -14,3 +16,14 @@ fi
 source ./venv/bin/activate
 
 pip install -r requirements.txt
+
+# Download the BioWordVec Model
+
+if [ ! -d  "$USER_SCRATCH" ]; then
+  export USER_SCRATCH="${HOME}/scratch"
+  mkdir "$USER_SCRATCH"
+fi
+
+if [ ! -f "$USER_SCRATCH/BioWordVec_PubMed_MIMICIII_d200.bin" ]; then
+  wget -P "$USER_SCRATCH" https://ftp.ncbi.nlm.nih.gov/pub/lu/Suppl/BioSentVec/BioWordVec_PubMed_MIMICIII_d200.bin
+fi
