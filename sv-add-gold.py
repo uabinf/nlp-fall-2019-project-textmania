4d67ede46db614622b78d89bbd3a1a7b529ae26d
Tobias O'Leary <tobiasoleary@mac.com>
2019-12-01 13:08:56 -0500

    Files for adding human classified Single Ventricle
    Adds SV-Gold-Simplified.xlsx explanation below.
    
    GOLD STANDARD.xlsx contains a list of rows that should have SV_GROUP set to 1.
    To get this group you had to manual filter by a highlighted color on three different rows.
    Since this is not the ideal format for a machine to read, created SV-GOLD-SIMPLIFIED.xlsx
    This is the two columns of the GOLD STANDARD for all the patients that should have SV_GROUP set to 1.
    
    Adds sv-add-gold.py
    
    sv-add-gold.py reads the SV-Gold-Simplified.xlsx and updates the SV_GROUP column
    from txpl_project.xlsx that should have been set to 1 based on human reviewed data in SV-Gold-Simplified.xlsx.
    
    Adds txpl_project_updates.xlsx
    
    This is the output of sv-add-gold. SV_GROUP values that should be 1 based on textual data that
    a human reviewed have been changed from 0 to 1. 101 rows were updated.

---
+++ sv-add-gold.py
@@ -0,0 +1,67 @@
+#sv-classifier.py
+
+# This file identifies instances of Single Ventricle using the Levenshtein.
+# Believe a simple tool like this could aid medical workers in finding the majority of cases in unstructured data.
+
+import sys
+import os
+
+import pandas as pd
+import math
+import re
+
+from pandas import ExcelFile
+
+
+ 
+GOLD_STANDARD = f"GoldStandard-Filtered.xlsx"
+INPUT_FILE = f"txpl_project.xlsx"
+OUTPUT_FILE = f"txpl_project_updated.xlsx"
+print(f"Reading {GOLD_STANDARD}")
+gold_df = pd.read_excel(GOLD_STANDARD)
+print(f"Gold Standard Shape: {gold_df.shape}")
+
+print(f"Reading {INPUT_FILE}")
+txpl_df = pd.read_excel(INPUT_FILE)
+
+sv_gold = txpl_df['PATIENT_ID'].apply(lambda x: 1 if x in pd.to_numeric(gold_df['PTID']).unique() else 0)
+
+
+def print_intersecting_patient_ids():
+  for i, v in enumerate(zip(sv_gold, txpl_df['SV_GROUP'], txpl_df['PATIENT_ID'])):
+    if v[0] == 1 and v[1] == 1:
+      print(f"SV_GOLD & SV_GROUP == 1 for PATIENT_ID: {v[2]}")
+# print_intersecting_patient_ids()
+
+print(f"SV_GOLD Count: {sv_gold.sum()}" )
+print(f"SV_GROUP Count: {txpl_df['SV_GROUP'].sum()}")
+
+sv_combined = sv_gold | txpl_df['SV_GROUP']
+sv_intersect = sv_gold & txpl_df['SV_GROUP']
+
+print(f"Insection Count: {sv_intersect.sum()}")
+print(f"Combined Count: {sv_combined.sum()}")
+
+txpl_df['SV_GROUP'] = sv_combined
+txpl_df.to_excel(OUTPUT_FILE, index=False)
+print(f"Wrote Updated txpl_df to {OUTPUT_FILE}")
+
+
+
+
+
+
+# match_idx = set()
+# stk = StanfordTokenizer() #Dependencies in the lib folder, ADD IT TO YOUR `CLASSPATH` env variable
+# for col_name in ["SPECOTH", "CHD_OTHSP"]:
+#   for index, item in enumerate(df[col_name]):
+#     if type(item) is str:
+#       if(matches_heterotaxy_words(stk.tokenize(item))):
+#         match_idx.add(index)
+#
+# print(f"Identified extra {len(match_idx)} cases of heterotaxy.")
+#
+# for i in match_idx:
+#   df.at[i, "HETEROTAXY"] = 1
+#
+# df.to_excel(OUTPUT_FILE)
